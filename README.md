e9a6780fb320b1d82da83cb0f4da4122f01fee12
kevin <kevin.carlotto@gmail.com>
2023-09-13 13:15:44 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,32 @@
+## Project Name
+
+change of year in php
+
+## Table of Contents
+
+    Usage
+    Contribution
+    License
+
+## Usage
+
+You can copy the php code where you copy the year 
+of the server and replicate it where you 
+paste the code, having to be in the copyright
+
+## Contribution
+
+Used for educational purposes, contributions from the community are welcome. 
+Collaborators can contribute by reporting bugs, proposing improvements, and sending pull requests.
+
+    Fork this repository.
+    Create a new branch: git checkout -b new-feature.
+    Make your changes and commit them: git commit -m 'Add new feature'.
+    Push your changes to the remote repository: git push origin new-feature.
+    Send a pull request.
+
+## License
+
+This project is licensed under the MIT License - see the LICENSE file for more details.
+
+© 2023 | KY C.O
---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- SEO Meta Tags -->
+    <meta name="description" content="Aria is a business focused HTML landing page template built with Bootstrap to help you create lead generation websites for companies and their services.">
+    <meta name="author" content="">
+
+    <!-- OG Meta Tags to improve the way the post looks when you share the page on LinkedIn, Facebook, Google+ -->
+	<meta property="og:site_name" content="" /> <!-- website name -->
+	<meta property="og:site" content="" /> <!-- website link -->
+	<meta property="og:title" content=""/> <!-- title shown in the actual shared post -->
+	<meta property="og:description" content="" /> <!-- description shown in the actual shared post -->
+	<meta property="og:image" content="" /> <!-- image link, make sure it's jpg -->
+	<meta property="og:url" content="" /> <!-- where do you want your post to link to -->
+	<meta property="og:type" content="article" />
+
+    <!-- Website Title -->
+    <title>Aria - Business HTML Landing Page Template</title>
+
+    <!-- Styles -->
+	<link href="css/styles.css" rel="stylesheet">
+
+	<!-- Favicon  -->
+    <link rel="icon" href="#">
+</head>
+<body data-spy="scroll" data-target=".fixed-top">
+
+
+    <!-- Copyright year Automatic-->
+    <div class="copyright">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <p>
+                        Copyright &copy; <?php echo date("Y"); ?> <a href="#your link"> - The Company Name</a> <!-- change of year in php -->
+                    </p>
+                </div> <!-- end of col -->
+            </div> <!-- enf of row -->
+        </div> <!-- end of container -->
+    </div> <!-- end of copyright -->
+    <!-- end of copyright -->
+
+
+    <!-- Scripts -->
+    <script src="#"></script> <!-- your scripts -->
+</body>
+</html>
